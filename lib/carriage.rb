98dfa7a28eb1d896bc4167443c1eb17eb364d8e1
Giorgia <giorgia1london@gmail.com>
2014-11-04 18:11:49 +0000

    carriage

--- lib/carriage.rb
+++ lib/carriage.rb
@@ -1,5 +1,18 @@
 class Carriage
 
+	attr_accessor :coach
+
+	def initialize
+		@coach = []
+	end
+
+	def empty?
+		@coach.empty?
+	end
+
+	# def to_have_capacity
+
+	# end
 
 
 end
--- spec/carriage_spec.rb
+++ spec/carriage_spec.rb
@@ -1,6 +1,21 @@
 require 'carriage'
 
-describe Carriage do 
+describe Carriage do
+
+let(:carriage)  {  Carriage.new } 
+
+it 'is not full when created' do
+	expect(carriage).to be_empty
+end
+
+# it 'has a default capacity when created' do
+# 	expect(carriage).to_have_capacity
+# end
+
+it 'has a default capacity that cannot be exceeded'
+
+
+it 'is part of the train'
 
 
 
@@ -10,6 +25,4 @@ describe Carriage do
 
 
 
-
-	
 end
